fa508e387c56cfac56549430b9563bcb526a45c7
Lemonade12 <plus2829@gmail.com>
2022-08-21 19:21:04 +0900

    Fix : statuscode 정리, 서비스단 조건 처리

--- src/opening/services/opening.js
+++ src/opening/services/opening.js
@@ -2,7 +2,7 @@ const openingRepo = require('../repos/opening');
 
 async function createOpening(req, res) {
     try {
-      const { company_id, position, compensation, content, skill } = req.query;
+      const { company_id, position, compensation, content, skill } = req.body;
       openingRepo.createOpening(company_id, position, compensation, content, skill);
       return res.status(201).json({ message : 'OPENING IS CREATED' });
     } catch (err) {
@@ -15,10 +15,17 @@ async function updateOpening(req, res) {
   try {
     const opening_id = req.params.id;
     const update = req.body;
-    console.log(opening_id);
-    console.log(update);
-    openingRepo.updateOpening(opening_id, update);
-    return res.status(200).json({ message : 'OPENING IS UPDATED' });
+    const opening = await openingRepo.readOpeningById(opening_id);
+    if(!opening){
+      return res.status(404).json({ message : 'OPENING IS NOT FOUND' });
+    }
+    else if(req.body.company_id){
+      return res.status(403).json({ message : 'CAN NOT UPDATE COMPANY_ID' });
+    }
+    else{
+      openingRepo.updateOpening(opening_id, update);
+      return res.status(200).json({ message : 'OPENING IS UPDATED' });
+    }  
   } catch (err) {
     console.log(err);
     return res.status(err.statusCode || 500).json({ message: err.message });
@@ -61,24 +68,27 @@ async function readOpening(req, res) {
 
 async function readOpeningDetail(req, res) {
   try {
-    const opening_id = req.params.id;
+    const opening_id = req.params.id;//not found
     const detail = await openingRepo.readOpeningDetail(opening_id);
-    const list = await openingRepo.readOpeningByCompanyId(detail.company.id, opening_id);
-    let arr = list.map(a=>a.id);
-    //해당 채용공고 id는 빠져야한다
-    const data = {
-      '채용공고_id': detail.id,
-      '회사명': detail.company.name,
-      '국가': detail.company.country,
-      '지역': detail.company.region,
-      '채용포지션': detail.position,
-      '채용보상금': detail.compensation,
-      '사용기술': detail.skill,
-      '채용내용': detail.content,
-      '회사가올린다른채용공고': arr,
+    if(detail){
+      const list = await openingRepo.readOpeningByCompanyId(detail.company.id, opening_id);
+      let arr = list.map(a=>a.id);
+      const data = {
+        '채용공고_id': detail.id,
+        '회사명': detail.company.name,
+        '국가': detail.company.country,
+        '지역': detail.company.region,
+        '채용포지션': detail.position,
+        '채용보상금': detail.compensation,
+        '사용기술': detail.skill,
+        '채용내용': detail.content,
+        '회사가올린다른채용공고': arr,
+      }
+      return res.status(200).json(data);
     }
-    
-    return res.status(200).json(data);
+    else{
+      return res.status(404).json({ message : 'OPENING IS NOT FOUND' });
+    }  
   } catch (err) {
     console.log(err);
     return res.status(err.statusCode || 500).json({ message: err.message });
